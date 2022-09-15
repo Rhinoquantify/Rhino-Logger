bf20c595d6696f3296f4ac6a33f9b3affd34c93f
benpaodewoniu <1506316219@qq.com>
2022-09-15 11:41:37 +0800

    增加 logger

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+
+setup(
+    name="RhinoLogger",
+    version="0.0.1",
+    description="The Rhino quantify of Logger",
+    url="",
+    author="XiNiu",
+    author_email="xiniublog@163.com",
+    license="GPL",
+    packages=[
+        "RhinoLogger",
+    ]
+)
